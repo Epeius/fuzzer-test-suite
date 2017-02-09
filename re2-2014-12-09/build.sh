4ca2d5a0be4466440273a6003980ac884ee24d1b
Epeius <binzhang08d@gmail.com>
2017-02-09 14:59:04 +0100

    re2-2014-12-09: build wrapper binary for non-LibFuzzer tools

--- re2-2014-12-09/build.sh
+++ re2-2014-12-09/build.sh
@@ -11,5 +11,4 @@ build_lib() {
 
 get_git_revision https://github.com/google/re2.git 499ef7eff7455ce9c9fae86111d4a77b6ac335de SRC
 build_lib
-build_libfuzzer
-clang++ -g $SCRIPT_DIR/target.cc -I  BUILD BUILD/obj/libre2.a libFuzzer.a  $FUZZ_CXXFLAGS -o $EXECUTABLE_NAME_BASE
+clang++ -DMAIN -g $SCRIPT_DIR/target.cc -I  BUILD BUILD/obj/libre2.a $FUZZ_CXXFLAGS -pthread -o re2-wrapper-binary
