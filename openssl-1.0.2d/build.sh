9d799bc5fc3fd4e8d7eab5a126221d33fddc4bc5
Epeius <binzhang08d@gmail.com>
2017-02-09 14:43:26 +0100

    openssl-1.0.2d: build wrapper binary for non-LibFuzzer tools

--- openssl-1.0.2d/build.sh
+++ openssl-1.0.2d/build.sh
@@ -6,11 +6,10 @@
 build_lib() {
   rm -rf BUILD
   cp -rf SRC BUILD
-  (cd BUILD && ./config && make clean && make CC="clang $FUZZ_CXXFLAGS"  -j $JOBS)
+  (cd BUILD && ./config -lmd && make clean && make CC="clang $FUZZ_CXXFLAGS"  -j $JOBS)
 }
 
 get_git_tag https://github.com/openssl/openssl.git OpenSSL_1_0_2d SRC
 build_lib
-build_libfuzzer
 set -x
-clang++ -g $SCRIPT_DIR/target.cc -DCERT_PATH=\"$SCRIPT_DIR/\"  $FUZZ_CXXFLAGS BUILD/libssl.a BUILD/libcrypto.a libFuzzer.a -lgcrypt -o $EXECUTABLE_NAME_BASE -I BUILD/include
+clang++ -DMAIN -g $SCRIPT_DIR/target.cc -DCERT_PATH=\"$SCRIPT_DIR/\"  $FUZZ_CXXFLAGS BUILD/libssl.a BUILD/libcrypto.a -lgcrypt -o openssl-1.0.2d-wrapper-binary -I BUILD/include
